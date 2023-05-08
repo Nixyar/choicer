a73a904c5332fe634a83129c247a7be0a89074af
Oleg K <fixyarik@yandex.ru>
2023-05-08 18:58:24 +0300

    d

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,19 +1,69 @@
-import React from 'react'
+import React, {useEffect} from 'react'
 import './Login.css';
+import {useNavigate} from 'react-router';
 import Logo from '../../img/logo(dark).svg';
 import LogoText from '../../img/text-logo(dark).png';
-import {getCookie} from '../../utils/u.utils';
+import {ITokenResponse} from '../../interfaces/login.interface';
+import axios from 'axios';
+import {getCookie, setCookie} from 'react-use-cookie';
+import {routes} from '../../Root';
 
 const crypto = require('crypto');
 
 export const Login = () => {
+  const navigate = useNavigate();
+
+  useEffect(() => {
+    if (getCookie('access_token')) {
+      getUserInfo().then();
+    } else {
+      const urlSearchParams = new URLSearchParams(window.location.search);
+      const code = urlSearchParams.get('code');
+      if (code) {
+        getToken(code).then();
+      }
+    }
+  }, []);
+
   const onLogin = () => {
     let nonce = getCookie('nonce');
     if (!nonce) {
       nonce = crypto.randomBytes(32).toString('hex');
       document.cookie = `nonce=${nonce}`;
     }
-    window.open(`https://oauth.mail.ru/xlogin?client_id=47beccc8bd8c4b2ba04b08c332d4b2d0&response_type=code&scope=&redirect_uri=https%3A%2F%2Fmaster--snazzy-palmier-903703.netlify.app%2Flogin&state=${nonce}`, '_blank');
+    window.open(
+        `https://oauth.mail.ru/xlogin?client_id=47beccc8bd8c4b2ba04b08c332d4b2d0&response_type=code&scope=&redirect_uri=https%3A%2F%2Fmaster--snazzy-palmier-903703.netlify.app%2Flogin&state=${nonce}`);
+  }
+
+  async function getToken(code: string) {
+    try {
+      const response: ITokenResponse = await axios.post('https://oauth.mail.ru/token', {
+        grant_type: 'authorization_code',
+        code: code,
+        redirect_uri: 'https://master--snazzy-palmier-903703.netlify.app/login',
+      }, {
+        auth: {
+          username: '47beccc8bd8c4b2ba04b08c332d4b2d0',
+          password: '2c289f33abb246ec92d9a1df8cb8cac7',
+        },
+      });
+      setCookie('access_token', response.access_token);
+      setCookie('refresh_token', response.refresh_token, {days: 30});
+      getUserInfo().then();
+    } catch (error) {
+      console.error(error);
+    }
+  }
+
+  async function getUserInfo() {
+    const access_token = getToken('access_token');
+    try {
+      const response = await axios.get(`https://oauth.mail.ru/userinfo?access_token=${access_token}`);
+      console.log(response);
+      navigate(routes.works);
+    } catch (error) {
+      console.error(error);
+    }
   }
 
   return (
