385a6f3ce248d827c47fcb579e940db2a0dac295
Oleg K <fixyarik@yandex.ru>
2023-05-08 17:19:07 +0300

    f

--- src/pages/Login/Login.tsx
+++ src/pages/Login/Login.tsx
@@ -1,13 +1,24 @@
 import React from 'react'
 import './Login.css';
-import Logo from "../../img/logo(dark).svg";
-import LogoText from "../../img/text-logo(dark).png";
+import Logo from '../../img/logo(dark).svg';
+import LogoText from '../../img/text-logo(dark).png';
+import {getCookie} from '../../utils/u.utils';
+
 const crypto = require('crypto');
 
 export const Login = () => {
   const onLogin = () => {
-    const nonce = crypto.randomBytes(32).toString('hex');
-    document.location.href = `https://oauth.mail.ru/xlogin?client_id=47beccc8bd8c4b2ba04b08c332d4b2d0&response_type=code&scope=&redirect_uri=https%3A%2F%2Fmaster--snazzy-palmier-903703.netlify.app%2Flogin&state=${nonce}`;
+    let nonce = getCookie('nonce');
+    if (!nonce) {
+      nonce = crypto.randomBytes(32).toString('hex');
+      document.cookie = `nonce=${nonce}`;
+    }
+    fetch(`https://oauth.mail.ru/xlogin?client_id=47beccc8bd8c4b2ba04b08c332d4b2d0&response_type=code&scope=&redirect_uri=https%3A%2F%2Fmaster--snazzy-palmier-903703.netlify.app%2Flogin&state=${nonce}`)
+        .then(response => console.log(response))
+        .then(data => {
+          // Обработка полученных данных
+        })
+        .catch(error => console.log(error));
   }
 
   return (
@@ -18,20 +29,21 @@ export const Login = () => {
             <img src={LogoText} alt="choicer."/>
           </div>
           <h1 className="login-information__title h1">О сервисе</h1>
-          <p className="login-information__text p2--light">Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
-            Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+          <p className="login-information__text p2--light">Amet minim mollit non deserunt ullamco
+            est sit aliqua dolor do amet sint.
+            Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt
+            nostrud amet.
           </p>
           <p className="p2--light">
             Amet minim mollit non deserunt ullamco est sit aliqua dolor do amet sint.
-            Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt nostrud amet.
+            Velit officia consequat duis enim velit mollit. Exercitation veniam consequat sunt
+            nostrud amet.
           </p>
         </div>
         <div className="login-form">
           <h1 className="login-form__title h1">Добро пожаловать!</h1>
           <form>
-            <a href="https://oauth.mail.ru/login">
-              <button onClick={onLogin}>Вход через Mail.ru</button>
-            </a>
+            <button onClick={onLogin}>Вход через Mail.ru</button>
           </form>
         </div>
       </div>
