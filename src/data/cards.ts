bc6237b4d2f448c371301219f8688e4f0753b4db
Oleg K <fixyarik@yandex.ru>
2023-05-08 14:52:15 +0300

    f

--- src/data/cards.ts
+++ src/data/cards.ts
@@ -16,4 +16,21 @@ export const cards = [
     files: [],
     isTakeToWork: true
   },
+  {
+    id: 2,
+    title: 'Автоматизация переработки сырья',
+    curator: {
+      photo: '',
+      fullName: 'Захаренко Андрей Альбертович'
+    },
+    description: 'Предлагаю к просмотру следущее видео по ссылке, чтобы подробно рассмотреть этот процесс.',
+    editedDays: 5,
+    participants: null,
+    type: 'курсовая',
+    endOfDate: '2023-05-16',
+    size: 16,
+    isRequired: true,
+    files: [],
+    isTakeToWork: true
+  },
 ];
