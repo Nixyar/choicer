4ce638c424bbaa8f3fed5f277589dd6607ac37c5
Oleg K <fixyarik@yandex.ru>
2023-05-09 11:31:37 +0300

    f

--- proxy.js
+++ proxy.js
@@ -12,7 +12,7 @@ app.use('/api', createProxyMiddleware({
   changeOrigin: true,
   pathRewrite: { '^/api': '/token' },
   onProxyRes: function(proxyRes, req, res) {
-    proxyRes.headers['Access-Control-Allow-Origin'] = 'https://master--snazzy-palmier-903703.netlify.app/';
+    proxyRes.headers['Access-Control-Allow-Origin'] = 'https://master--snazzy-palmier-903703.netlify.app';
     proxyRes.headers['Access-Control-Allow-Methods'] = 'GET, POST, PUT, DELETE, OPTIONS';
     proxyRes.headers['Access-Control-Allow-Headers'] = 'X-Requested-With, Content-Type, Authorization';
   }
