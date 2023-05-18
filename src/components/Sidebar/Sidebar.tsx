8dc64022fdc54615a25f5fadf0d0b92b75c2f9e2
Oleg K <fixyarik@yandex.ru>
2023-05-19 00:19:19 +0300

    f

--- src/components/Sidebar/Sidebar.tsx
+++ src/components/Sidebar/Sidebar.tsx
@@ -44,7 +44,19 @@ export const Sidebar = () => {
 
   const onLogout = () => {
     store.dispatch(setUserName(initialState));
+    deleteAllCookies();
     navigate(routes.login);
+  }
+
+  const deleteAllCookies = () => {
+    const cookies = document.cookie.split(";");
+
+    for (let i = 0; i < cookies.length; i++) {
+      let cookie = cookies[i];
+      let eqPos = cookie.indexOf("=");
+      let name = eqPos > -1 ? cookie.substr(0, eqPos) : cookie;
+      document.cookie = name + "=;expires=Thu, 01 Jan 1970 00:00:00 GMT";
+    }
   }
 
   return (
