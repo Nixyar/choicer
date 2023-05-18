e0ff0528e2794af6ef6904ddfde59511180f29a5
Oleg K <fixyarik@yandex.ru>
2023-05-18 21:35:09 +0300

    f

--- netlify/functions/token.ts
+++ netlify/functions/token.ts
@@ -8,8 +8,6 @@ const handler: Handler = async (event: HandlerEvent, context: HandlerContext) =>
     const body = {
       code: code,
       redirect_uri: 'https://snazzy-palmier-903703.netlify.app/login',
-      client_id: 'bfc815235bb84333947c6a44e91684cd',
-      client_secret: 'b456b855c91648c4a3a59806bd0c4769',
       grant_type: 'authorization_code'
     };
     const response = await axios.post('https://oauth.mail.ru/token', body);
