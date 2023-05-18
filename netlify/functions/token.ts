9d73ec2fbde0c517977d8191ce2250d5006301a2
Oleg K <fixyarik@yandex.ru>
2023-05-18 21:17:22 +0300

    f

--- netlify/functions/token.ts
+++ netlify/functions/token.ts
@@ -1,24 +1,21 @@
 import { Handler, HandlerEvent, HandlerContext } from "@netlify/functions";
 import axios from 'axios';
 
-interface ITokenBody {
-  code: string;
-}
-
 const handler: Handler = async (event: HandlerEvent, context: HandlerContext) => {
   const code: string = JSON.parse(event.body + '').code;
 
   try {
     const body = {
-      code,
-      redirect_uri: 'https://master--snazzy-palmier-903703.netlify.app/login',
+      code: code,
+      redirect_uri: 'https://snazzy-palmier-903703.netlify.app/login',
       client_id: 'bfc815235bb84333947c6a44e91684cd',
-      client_secret: 'b456b855c91648c4a3a59806bd0c4769'
+      client_secret: 'b456b855c91648c4a3a59806bd0c4769',
+      grant_type: 'authorization_code'
     };
     const response = await axios.post('https://oauth.mail.ru/token', body);
     return {
       statusCode: 200,
-      body: JSON.stringify(body),
+      body: JSON.stringify(response.data),
     };
   } catch (error) {
     console.error(error);
